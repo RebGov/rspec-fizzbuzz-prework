d0e53c808dcd60cfd0196a662ce221c049afacc7
Rebecca Govert <becci.govert@gmail.com>
2018-05-21 16:36:07 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 
 def fizzbuzz(int)
- if int % 3 == 0 # if the number int is divisible by 3
-  "Fizz" # Go fizz
+ if int % 3 && 5 == 0 # if the number int is divisible by 3
+  "FizzBuzz" # Go fizz
  else if int % 5 == 0 
   "Buzz"
- else if int % 3 && 5 == 0
-   "FizzBuzz"
+ else if int % 3 == 0
+   "Fizz"
  end
 end
 end
