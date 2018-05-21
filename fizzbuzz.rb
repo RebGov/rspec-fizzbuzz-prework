78a66cfd9ce1591bf0ca6e76f21bb89230b78652
Rebecca Govert <becci.govert@gmail.com>
2018-05-21 16:38:31 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 
 def fizzbuzz(int)
- if int % 3 == 0  && int % 5 == 0 # if the number int is divisible by 3 and 5
+ if int % 3 == 0  && int % 5 == 0 # if the number int is divisible by 3 and 5. put first so that not return "Fizz" or "Buzz" as passes this condition first before moving to next
   "FizzBuzz" # Go fizz
  else if int % 5 == 0 # if the number int is divisible by 5
   "Buzz"
